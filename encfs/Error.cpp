e66fc337a4b648ae160a57dc5ea1f3e73b341d33
Valient Gough <vgough@pobox.com>
2015-06-17 20:33:57 -0700

    start moving to glog

---
+++ encfs/Error.cpp
@@ -0,0 +1,7 @@
+#include "base/Error.h"
+
+namespace encfs {
+
+Error::Error(const char *msg) : runtime_error(msg) {}
+
+}  // namespace encfs
---
+++ encfs/Error.h
@@ -0,0 +1,28 @@
+#ifndef _Error_incl_
+#define _Error_incl_
+
+// Provides compatibility with RLog's rAssert, which throws an Error exception.
+
+#include <glog/logging.h>
+#include <stdexcept>
+
+namespace encfs {
+
+class Error : public std::runtime_error {
+ public:
+  Error(const char *msg);
+};
+
+#define STR(X) #X
+
+#define rAssert(cond)                               \
+  do {                                              \
+    if ((cond) == false) {                          \
+      LOG(ERROR) << "Assert failed: " << STR(cond); \
+      throw Error(STR(cond));                       \
+    }                                               \
+  } while (0)
+
+}  // namespace encfs
+
+#endif
